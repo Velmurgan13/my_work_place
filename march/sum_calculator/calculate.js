6edafec62325474461d3c2a54c3118cd108de2e2
velmurgan pillai <karthikpillai1323@gmail.com>
2021-03-17 23:36:21 +0530

    not imp changes

--- march/sum_calculator/calculate.js
+++ march/sum_calculator/calculate.js
@@ -1,11 +1,27 @@
-function calculate() {
-var field1 = document.getElementById("Num 1").value;
-var field2 = document.getElementById("Num 2").value;
+var btn = document.querySelector(".submit");
+btn.addEventListener("click",function calculator() {
+    var Num1 = parseInt(document.getElementById("Number1").value);
+    var Num2 = parseInt(document.getElementById("Number2").value);
+    var oporator = document.getElementById("oporator").value;
+    var ans = document.getElementById("ans");
+    if (oporator == "sum") {
+        var sum = Num1 + Num2
+        ans.innerText = sum;
+    }
+    else if (oporator == "substract") {
+        var substract = Num1 - Num2
+        ans.innerText = substract;
+    }
+    else if (oporator == "division") {
+        var division = Num1 / Num2
+        ans.innerText = division;
+    }
+    else if (oporator == "select") {
+        alert("Choose proper option");
 
-var result = parseFloat(field1) + parseFloat(field2);
-
-if (!isNaN(result)) {
-    document.getElementById("answer").innerHTML =
-    "The Answer is " + result;
-}
-}
+    }
+    else {
+        var multiply = Num1 * Num2
+        return ans.innerText = multiply;
+    }
+})
