6b3ad9f46e0f1373c54fda56aa1a7043d68d9963
velmurgan pillai <karthikpillai1323@gmail.com>
2021-07-06 10:01:45 +0530

    revision of html and css

---
+++ july/basic/src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <ul className="left-bar">
+          <li>
+            <a href=""> Logo </a>
+          </li>
+        </ul>
+        <ul className="right-bar">
+          <li>
+            <a href=""> Services </a>
+          </li>
+          <li>
+            <a href=""> About Us </a>
+          </li>
+          <li>
+            <a href=""> Contact Us </a>
+          </li>
+        </ul>
+      </header>
+      <footer className="footer">
+        <h4>All Copy Right Reserved</h4>
+      </footer>
+    </>
+  );
+}
+
+export default App;
